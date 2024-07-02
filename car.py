9bbc6d245a8c7e0bb9f62e0ac0548d636b0c3920
TheContentMaker3000 <96380370+YeetTeamYT@users.noreply.github.com>
2024-07-02 12:02:15 +0100

    Create car.py

---
+++ car.py
@@ -0,0 +1,16 @@
+class Car:
+
+def __init__(self,make,model,year,color):
+  self.make = make
+  self.model = model
+  self.year = year
+  elf.color = color
+
+def = drive(self):
+  print("This Car is Driving")
+
+def stop(self):
+  print("This Car is Stopped")
+  
+  
+  
